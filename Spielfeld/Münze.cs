c938833beaf2aadbffba05bd7a94fb2d9926fc85
entity121 <heck.basti@gmail.com>
2022-08-29 13:48:50 +0200

    29.08.2022 #1

---
+++ Spielfeld/Münze.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTCG.Spielfeld
+{
+    class Münze
+    {
+
+        public bool Münzwurf()
+        {
+            Random random = new Random();
+
+            int ergebnis = random.Next(0,2);
+
+            if(ergebnis == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+    }
+}
